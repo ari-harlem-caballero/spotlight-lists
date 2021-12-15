ecc79510e90a3331132ca6981311efe3d6c729a3
Ari Harlem-Caballero <ari.bash.harlem@gmail.com>
2021-12-14 17:13:49 -0800

    completed render foods

--- render-food.js
+++ render-food.js
@@ -8,6 +8,17 @@ export function renderFoods(food) {
 
     foodDiv.classList.add('food-hold');
 
+    foodName.classList.add('food-name');
+    foodName.textContent = food.name;
+
+    foodType.classList.add('food-type');
+    foodType.textContent = food.type;
+
+    foodInfo.classList.add('info');
+    foodInfo.textContent = `Total prep: ${food.prep_time}, Serve hot: ${food.serve_hot}`;
+
     foodInfo.append(foodPrep, foodHeat);
     foodDiv.append(foodName, foodType, foodInfo);
+
+    return foodDiv;
 }
